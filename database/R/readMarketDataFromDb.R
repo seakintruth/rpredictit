c5af5240a551d7d5f3c4cb107821857f44056734
Jeremy D. Gerdes <seakintruth@gmail.com>
2020-02-10 06:09:23 -0500

    Create readMarketDataFromDb.R

---
+++ database/R/readMarketDataFromDb.R
@@ -0,0 +1,47 @@
+pacman::p_load(RSQLite)
+project.dir <- file.path("/media","jeremy","250GbUsb","data","r","predictit")
+
+readClosedMarketDataFromDb <- function(){
+  db = DBI::dbConnect(
+    RSQLite::SQLite(), 
+    dbname=file.path(project.dir,"predictit.sqlite")
+  )
+  test.existing.closed.result <- RSQLite::dbSendQuery(
+    conn=db,
+    "SELECT * FROM market_observations WHERE Status LIKE 'Closed'"
+  )
+  test.existing.closed.data <-RSQLite::dbFetch(test.existing.closed.result)
+  dbClearResult(test.existing.closed.result)
+  RSQLite::dbDisconnect(db)
+  return(test.existing.closed.data)
+}
+
+readOpenMarketDataFromDb <- function(){
+  db = DBI::dbConnect(
+    RSQLite::SQLite(), 
+    dbname=file.path(project.dir,"predictit.sqlite")
+  )
+  test.existing.open.result <- RSQLite::dbSendQuery(
+    conn=db,
+    "SELECT * FROM market_observations WHERE Status LIKE 'Open'"
+  )
+  test.existing.open.data <-RSQLite::dbFetch(test.existing.open.result)
+  dbClearResult(test.existing.open.result)
+  RSQLite::dbDisconnect(db)
+  return(test.existing.open.data)
+}
+
+readNullIdMarketDataFromDb <- function(){
+  db = DBI::dbConnect(
+    RSQLite::SQLite(), 
+    dbname=file.path(project.dir,"predictit.sqlite")
+  )
+  null.id.result <- RSQLite::dbSendQuery(
+    conn=db,
+    "SELECT DISTINCT id FROM market_id_null"
+  )
+  null.id.data <-RSQLite::dbFetch(null.id.result)
+  dbClearResult(null.id.result)
+  RSQLite::dbDisconnect(db)
+  return(null.id.result)
+}
